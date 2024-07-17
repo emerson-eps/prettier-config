ea1137fef51e53397874d92fde05232709422825
Andreas Solem Kjær <andreassolem.kjaer@aspentech.com>
2024-07-17 13:53:33 +0200

    fix: setting trailingComma to es5

--- prettier.config.js
+++ prettier.config.js
@@ -10,6 +10,9 @@ module.exports = {
   // arrowParens default since Prettier v2.0.0 is "always"
   // "avoid" present to avoid reformatting pre v2.0 code.
   arrowParens: "avoid",
+  // trailingComma default since Prettier v3 is "all"
+  // setting to "es5" for nicer formatting of JSX and less formatting changes.
+  trailingComma: "es5",
   // Cypress overrides: allow longer lines
   overrides: [
     {
