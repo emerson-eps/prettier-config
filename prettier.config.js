7aa84574211759c660cb6374fdf30067ed786862
Andreas Solem Kjær <49904608+andreassk80@users.noreply.github.com>
2024-07-18 11:34:52 +0200

    fix: default to arrowParens=always (#43)

--- prettier.config.js
+++ prettier.config.js
@@ -7,9 +7,6 @@
 
 // https://prettier.io/docs/en/options.html
 module.exports = {
-  // arrowParens default since Prettier v2.0.0 is "always"
-  // "avoid" present to avoid reformatting pre v2.0 code.
-  arrowParens: "avoid",
   // trailingComma default since Prettier v3 is "all"
   // setting to "es5" for nicer formatting of JSX and less formatting changes.
   trailingComma: "es5",
